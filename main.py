246ed88e3dbfcb49d0945c8fbafafb0fa59f6d13
Nytri <llTheBlankll@gmail.com>
2022-04-17 13:35:56 +0800

    listAllBlockedProcesses() method done.

--- main.py
+++ main.py
@@ -41,11 +41,11 @@ def main():
             listAllBlockedProcesses()
         else:
             print(f"Invalid input number {val}")
-            time.sleep(2)
+            time.sleep(1)
             main()
     except ValueError:
         print("Only numbers are allowed.")
-        time.sleep(2)
+        time.sleep(1)
         main()
 
 
@@ -63,7 +63,7 @@ def addProcess():
     
     if process_name == "":
         print("Process name cannot be empty.")
-        time.sleep(2)
+        time.sleep(1)
         main()
     elif process_name == "back":
         main()
@@ -76,7 +76,7 @@ def addProcess():
                 if process_name == proc.pid:
                     print(f"Process {proc.name()} with the PID of {proc.pid} is blocked successfully.")
                     list_of_blocked_processes.append(proc)
-                    time.sleep(2)
+                    time.sleep(1)
                     # * Go back to Main menu
                     main()
         else:
@@ -85,36 +85,37 @@ def addProcess():
                 if process_name == str(proc.name()):
                     print(f"Process {proc.name()} is blocked successfully.")
                     list_of_blocked_processes.append(proc)
-                    time.sleep(2)
+                    time.sleep(1)
                     # * Go back to Main menu
                     main()
         # * If no process was found, the program won't go to main menu, see the code above. 
         # * So we will raise an alert to say that no program was found and ask the user again for what the program to block.
         print(f"Process {process_name} was not found.")
-        time.sleep(2)
+        time.sleep(1)
         addProcess()
 
 
 def clearAllBlockedProcesses():
     list_of_blocked_processes.clear()
     print("Processes cleared successfully.")
-    time.sleep(2)
+    time.sleep(1)
     main()
 
 
 def listAllBlockedProcesses():
-    pass
-    
-    
+    ptable = prettytable.PrettyTable(field_names=["PID", "Process name"])
+    for proc in list_of_blocked_processes:
+        ptable.add_row([proc.pid, proc.name()])
+    print(ptable)
+    input("Please enter to continue.")
+    main()
 
 
 def startBlocking():
     if len(list_of_blocked_processes) == 0:
         print("Please specify a blocked processes.")
-        time.sleep(2)
+        time.sleep(1)
         main()
-    
-    
 
 
 if __name__ == "__main__":
